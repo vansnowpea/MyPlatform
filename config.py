1c0185e3141b63c3e95ad9f21ef2ff294f0e0b48
vansnowpea <409085788@qq.com>
2016-08-30 23:29:11 +0800

    2016/08/30 周二23:29:11.48

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #coding=utf8
 
-TOKEN = ''
-APP_ID = ''
-SECRET_KEY = ''
-TULING_KEY = ''
+TOKEN = '9g6qhQ3Tud555MB'
+APP_ID = 'wx1fe475b4554474ee'
+SECRET_KEY = '169b8bacb3e703f4063dee755f35bb7b'
+TULING_KEY = 'daa07de27fc64becb689c69c83a5e81f'
 ADMIN_OAUTH = 'admin_oauth'
 WELCOME_WORD = u'''\
 欢迎关注我的微信号！
@@ -11,7 +11,7 @@ WELCOME_WORD = u'''\
 目录： 获取文章目录的网址
 帮助： 获取本信息
 其他： 图灵机器人会陪你聊天'''
-INDEX_URL = 'www.baidu.com'
+INDEX_URL = 'van3.applinzi.com/articles_list/'
 REPLY_DICT = {
     u'目录': '点这里-> ' + INDEX_URL,
     u'帮助': WELCOME_WORD,
